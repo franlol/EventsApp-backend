72428bfce78a9d3effb05b137a56140d0be49e85
franlol <fraaaaan@gmail.com>
2020-09-28 11:53:47 +0200

    :clown_face: Adding moked Event for temporal :construction: graphql

--- src/app/app.ts
+++ src/app/app.ts
@@ -6,18 +6,53 @@ import { buildSchema } from 'graphql'
 const app: Express = express();
 dotenv();
 
-const eventList: Array<string> = ['hey', 'hoy'];
+type TEvent = {
+  _id: string;
+  title: string;
+  description: string;
+  price: number;
+  date: string;
+}
+
+const eventList: Array<TEvent> = [{
+  _id: 'id1',
+  title: 'Event title 1',
+  description: 'Event Description 1',
+  price: 1.5,
+  date: (new Date()).toString()
+}, {
+  _id: 'id2',
+  title: 'Event title 2',
+  description: 'Event Description 2',
+  price: 2.5,
+  date: (new Date()).toString()
+}];
 
 app.use(express.json());
 app.use('/graphql', graphqlHTTP({
   schema: buildSchema(`
-  
+    type Event {
+      _id: ID!
+      title: String!
+      description: String!
+      price: Float!
+      date: String!
+    }
+
+    input EventInput {
+      _id: String!
+      title: String!
+      description: String!
+      price: Float!
+      date: String!
+    }
+
     type RootQuery {
-      events: [String]
+      events: [Event!]!
     }
 
     type RootMutation {
-      createEvent(eventName: String): String
+      createEvent(eventInput: EventInput): Event
     }
 
     schema {
@@ -26,13 +61,16 @@ app.use('/graphql', graphqlHTTP({
     }
 
   `),
+
   rootValue: {
     events: () => eventList,
-    createEvent: ({ eventName }: { eventName: string }) => {
-      eventList.push(eventName);
-      return eventName
+    createEvent: ({ eventInput }: { eventInput: TEvent }) => {
+      eventList.push(eventInput);
+      console.log(eventInput)
+      return eventInput
     }
   },
+
   graphiql: true,
 }));
 
