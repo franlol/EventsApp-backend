ef794093e3b1e31a62e3f214858e0df01409abf1
franlol <fraaaaan@gmail.com>
2020-09-28 11:28:08 +0200

    :sparkles: Adding GraphQL middleware

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,39 @@
 import express, { Express } from "express";
 import { config as dotenv } from 'dotenv';
+import { graphqlHTTP } from 'express-graphql';
+import { buildSchema } from 'graphql'
 
 const app: Express = express();
 dotenv();
 
+const eventList: Array<string> = ['hey', 'hoy'];
+
 app.use(express.json());
+app.use('/graphql', graphqlHTTP({
+  schema: buildSchema(`
+  
+    type RootQuery {
+      events: [String]
+    }
+
+    type RootMutation {
+      createEvent(eventName: String): String
+    }
+
+    schema {
+      query: RootQuery
+      mutation: RootMutation
+    }
+
+  `),
+  rootValue: {
+    events: () => eventList,
+    createEvent: ({ eventName }: { eventName: string }) => {
+      eventList.push(eventName);
+      return eventName
+    }
+  },
+  graphiql: true,
+}));
 
 export default app;
